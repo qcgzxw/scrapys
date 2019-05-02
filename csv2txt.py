2962ab6545167ed3a27aba33b134baa7b797ed42
owen <admin@qcgzxw.cn>
2019-05-02 17:56:30 +0800

    优化未知字符，处理小说排序

--- csv2txt.py
+++ csv2txt.py
@@ -4,9 +4,9 @@ import pandas as pd
 books = pd.read_csv('book/test.csv')
 folder = '我的老千生涯'
 for index in range(0, len(books)):
-    title = books['title'][index] + '.txt.'
-    content = books['content'][index].replace('<br>', '\r\n').replace('\u3000', ' ')
-    with open(folder + '/' + title, 'w+') as txt:
-        txt.write(books['title'][index] + '\r\n')
+    title = books['title'][index].replace('\xa0', '')
+    content = books['content'][index].replace('<br>', '\r\n').replace('\u3000', ' ').replace('\xa0', '')
+    with open(folder + '/' + str(index) + title + '.txt.', 'w+') as txt:
+        txt.write(title + '\r\n')
         txt.write(content)
         txt.close()
