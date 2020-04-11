42f4c1d6ec9d2f5a2ada1e77b0590fbfd49e7528
Owen <qcgzxw@qq.com>
2020-04-11 22:09:08 +0800

    转换为命令行参数传入关键词

--- ip/ip/spiders/ipipnet.py
+++ ip/ip/spiders/ipipnet.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from ..items import IpItem
+from urllib.parse import quote
 
 
 class IpipnetSpider(scrapy.Spider):
     name = 'ipipnet'
     allowed_domains = ['whois.ipip.net']
     start_urls = ['https://whois.ipip.net/search/GOOGLE%20LLC']
+
+    def __init__(self, keyword=None, *args, **kwargs):
+        super(IpipnetSpider, self).__init__(*args, **kwargs)
+        if keyword:
+            if '%' not in keyword:
+                keyword = quote(keyword)
+            keyword = keyword.upper()
+            self.start_urls = ['https://whois.ipip.net/search/%s' % keyword]
 
     def parse(self, response):
         for urlLists in response.xpath('//table[@class="table"]/tr/td/a/@href').extract():
